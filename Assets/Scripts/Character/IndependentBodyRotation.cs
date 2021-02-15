9f18a6d7f706cc579ee230540f3dc72bbb13ae85
EonZeNx <sparck.va@gmail.com>
2021-02-15 14:25:10 +1300

    Formatting. Made player a prefab.

--- Assets/Scripts/Character/IndependentBodyRotation.cs
+++ Assets/Scripts/Character/IndependentBodyRotation.cs
@@ -7,40 +7,38 @@ public class IndependentBodyRotation : MonoBehaviour
     /// <summary>
     /// References to the independently rotating parts
     /// </summary>
-    public Transform upperBody;
-    public Transform lowerBody;
+    public Transform upper;
+    public Transform lower;
 
     public CharacterController controller;
     public Transform cam;
-
-    /// <summary>
-    /// Target vectors for both parts of the body
-    /// </summary>
-    public Vector3 targetUpperBodyVec;
-    public Vector3 targetLowerBodyVec;
 
     /// <summary>
     /// Smoothness for both velocity and time
     /// </summary>
     public float smoothVelUpperBody;
     public float smoothVelLowerBody;
-    public float smoothTimeUpperBody = 0.05f;
-    public float smoothTimeLowerBody = 0.03f;
+    public float smoothTimeUpperBody = 0.02f;
+    public float smoothTimeLowerBody = 0.07f;
 
     public void Update()
     {
-        float currentUpperBodyAngle = upperBody.transform.eulerAngles.y;
-        float currentLowerBodyAngle = lowerBody.transform.eulerAngles.y;
-        float lookAngle = cam.transform.eulerAngles.y;
+        // Get current angles
+        float currentUpperAngle = upper.transform.eulerAngles.y;
+        float currentLowerAngle = lower.transform.eulerAngles.y;
         
+        // Calculate target angles
+        float targetUpperAngle = cam.transform.eulerAngles.y;
         Vector3 playerVelocity = controller.velocity;
         playerVelocity.y = 0;
-        float playerVelocityAngle = Mathf.Atan2(playerVelocity.x, playerVelocity.z) * Mathf.Rad2Deg;
+        float targetLowerAngle = Mathf.Atan2(playerVelocity.x, playerVelocity.z) * Mathf.Rad2Deg;
 
-        float targetUpperBodyAngle = Mathf.SmoothDampAngle(currentUpperBodyAngle, lookAngle, ref smoothVelUpperBody, smoothTimeUpperBody);
-        float targetLowerBodyAngle = Mathf.SmoothDampAngle(currentLowerBodyAngle, playerVelocityAngle, ref smoothVelLowerBody, smoothTimeLowerBody);
+        // Smooth transitions
+        float targetUpperBodyAngle = Mathf.SmoothDampAngle(currentUpperAngle, targetUpperAngle, ref smoothVelUpperBody, smoothTimeUpperBody);
+        float targetLowerBodyAngle = Mathf.SmoothDampAngle(currentLowerAngle, targetLowerAngle, ref smoothVelLowerBody, smoothTimeLowerBody);
 
-        upperBody.transform.Rotate(Vector3.up, targetUpperBodyAngle - currentUpperBodyAngle);
-        lowerBody.transform.Rotate(Vector3.up, targetLowerBodyAngle - currentLowerBodyAngle);
+        // Apply angles
+        upper.transform.Rotate(Vector3.up, targetUpperBodyAngle - currentUpperAngle);
+        lower.transform.Rotate(Vector3.up, targetLowerBodyAngle - currentLowerAngle);
     }
 }
