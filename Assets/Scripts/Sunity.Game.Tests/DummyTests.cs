5d4b56fdb4555a59d634924ea525d5e2c7668504
Terence <terence@terencequ.com>
2021-03-27 14:49:06 +1300

    Major refactoring, code now goes in assemblies, test class

---
+++ Assets/Scripts/Sunity.Game.Tests/DummyTests.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+[TestFixture]
+public class DummyTests
+{
+    /// <summary>
+    /// Example unit test.
+    /// Use the naming scheme; Subject_State_Behaviour,
+    /// i.e. 
+    /// [Test]
+    /// public void AddNumbers_BothInputsPositive_ReturnsPositiveNumber() 
+    /// {
+    ///     Assert.IsTrue(AddNumbers(1, 1) > 0);
+    /// }
+    /// ... where the subject is the AddNumbers method
+    /// ... the state is that both inputs to AddNumbers are positive
+    /// ... and the behaviour is that AddNumbers will return a positive result.
+    /// </summary>
+    [Test]
+    public void Subject_State_Behaviour()
+    {
+        Assert.AreEqual(true, true);
+    }
+}
